bb35c24b510c54a160aa0fb36a0d94291820f54d
iam <iam@kaiz.vn>
2024-10-27 13:24:25 +0000

    second commit

---
+++ src/app/housing-location/housing-location.component.ts
@@ -0,0 +1,27 @@
+import { Component, Input } from '@angular/core';
+import {HousingLocation} from '../housinglocation';
+
+
+@Component({
+  selector: 'app-housing-location',
+  standalone: true,
+  imports: [],
+  template: `
+    <section class="listing">
+      <img
+        class="listing-photo"
+        [src]="housingLocation.photo"
+        alt="Exterior photo of {{ housingLocation.name }}"
+        crossorigin
+      />
+      <h2 class="listing-heading">{{ housingLocation.name }}</h2>
+      <p class="listing-location">{{ housingLocation.city }}, {{ housingLocation.state }}</p>
+    </section>
+  `,
+  styleUrls: ['./housing-location.component.css'],
+})
+export class HousingLocationComponent {
+
+  @Input() housingLocation!: HousingLocation;
+
+}
